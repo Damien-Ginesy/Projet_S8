0f14233ca178db9416cf4ad075d3a9a1f628da54
dginesy <dginesy@enseirb-matmeca.fr>
2022-03-11 01:57:02 +0100

    Mise en place de l'interface de connexion à la BDD

---
+++ serveur/src/db.ts
@@ -0,0 +1,37 @@
+import {Schema, model, Model, Document} from "mongoose";
+import mongoose from "mongoose";
+
+
+interface InfoNoeuds extends Document{
+    vue: Array<string>;
+}
+
+ export class DatabaseAccess {
+
+    private noeudSchema:Schema = new Schema({
+        vue: [{type: String, required: true}]
+    });
+
+    private noeudModel:Model<InfoNoeuds> = model<InfoNoeuds>('Info_Noeuds', this.noeudSchema);
+
+    constructor(user:string, password:string) {
+        const urlmongo = `mongodb+srv://${user}:${password}@test.bnuu4.mongodb.net/RéseauxData?retryWrites=true&w=majority`;
+        this.openDb(urlmongo);
+    }
+
+    addInfo(vue:Array<string>){
+        const noeud:InfoNoeuds = new this.noeudModel({
+            vue:vue
+        });
+        noeud.save();
+    }
+
+    private openDb(url: string) {
+        mongoose.connect(url);
+        const db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'Erreur lors de la connexion'));
+        db.once('open', () => {
+            console.log("Connexion à la base de donnée réussi");
+        });
+    }
+}
--- serveur/src/server.ts
+++ serveur/src/server.ts
@@ -1,13 +1,20 @@
 import express from 'express';
-
+import {DatabaseAccess} from './db'
 const app = express();
 const port = 3000;
 
 app.set('views','./views');
 app.set('view engine','pug');
 
-app.get('/',(req, res) =>{
+const db = new DatabaseAccess("Damien", "N3cozwDA20fg");
+db.addInfo(["P8","P7","P5"]);
+
+app.get('/acceuil',(req, res) => {
     res.render('homepage');
+})
+
+app.get('/',(req, res) =>{
+    res.redirect('/acceuil');
 });
 
 app.listen(port ,() => {
