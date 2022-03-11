266d6c3af2653b47d77facae9f197d195f6ac934
ImadABID <imad.abied@gmail.com>
2022-03-11 15:33:13 +0100

    adding inventory file

--- ansible/create_virtual_network.sh
+++ ansible/create_virtual_network.sh
@@ -2,16 +2,19 @@
 
 #remove host ip adresses file
 rm -f ip_adresses
+rm -f hosts
 
 # host 1
 docker run -itd \
 -v "$(pwd):/home/dev" \
 ansible;
+echo "172.17.0.2" >> hosts
 
 # host 2
 docker run -itd \
 -v "$(pwd):/home/dev" \
 ansible;
+echo "172.17.0.3" >> hosts
 
 # main machine
 docker run -it \
