12fd0e1800d40f5609a579152d3e9d7fa90d1385
Victor FALAISE <victor.falaise@enseirb-matmeca.fr>
2022-03-12 14:37:42 +0100

    1ere implementation update() & reset(), pas compilé

--- basalt_core/src/Node.cpp
+++ basalt_core/src/Node.cpp
@@ -2,5 +2,25 @@
 
 namespace Basalt
 {
+    void Node::update(Hash<HashSize> (*h)(const NodeId&, uint32_t seed)) {
+      NodeId p = selectPeer();
+      NodeId q = selectPeer();
+      pull(p);
+      push(q); //+ _view
+    }
+
+
+    Array<NodeId> Node::reset() {
+      Array<NodeId> samples = new Array<NodeId>(_k);
+      size_t v = _view.size();
+      for (size_t i = 0; i < _k; i++) {
+        _r = (_r%(v-1))+1;
+        samples[i] = _view[_r].id;
+        _view[_r].seed = randSeed();
+      }
+      updateSamples(_view);
+      return samples;
+    }
+
     
 } // namespace Basalt
