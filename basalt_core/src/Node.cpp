8b50f3417e79c12100d0b55656f1ac1da2593bd9
Ghariani Ayman <aghariani@bordeaux-inp.fr>
2022-03-12 15:09:24 +0100

    1ere implementation selectpeer() & updatesamples(), pas compilé

--- basalt_core/src/Node.cpp
+++ basalt_core/src/Node.cpp
@@ -2,6 +2,33 @@
 
 namespace Basalt
 {
+
+    void Node::updateSamples(const Array<NodeId>& candidates){
+         for(uint32_t i=0; i<_view.size() ; ++i){
+              uint32_t currentHash = RankFunc(_view[i].seed, _view[i].id);
+             for (NodeId p : candidates) {
+                if(_view[i].id == p){ _view[i].hits++; return; }
+                uint32_t P_Hash = RankFunc(_view[i].seed, _view[i].id);
+                if(_view[i].id == nullptr || P_Hash < currentHash){
+                    currentHash = P_Hash;
+                    _view[i].hits = 1;
+                    _view[i].id = p;
+                }
+             }
+         }
+
+    }
+
+
+    NodeId Node::selectPeer(){
+        uint32_t p=0;
+        for(uint32_t i=1; i<_view.size(); ++i)
+            p = _view[p].hits<_view[i].hits? p:i;
+        _view[p].hits++;
+        return _view[p].id;
+    }  
+
+    
     void Node::update(Hash<HashSize> (*h)(const NodeId&, uint32_t seed)) {
       NodeId p = selectPeer();
       NodeId q = selectPeer();
