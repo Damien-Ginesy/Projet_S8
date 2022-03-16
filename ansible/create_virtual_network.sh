96a9eb9f5cd1be190efdc2be4c8ae4615b5ff9c5
ImadAbid <imad.abied@gmail.com>
2022-03-16 12:15:29 +0100

    ssh connection using rsa keys

--- ansible/create_virtual_network.sh
+++ ansible/create_virtual_network.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# En cas de modification de nbr de hosts
+#    * create_virtual_network.sh : echo "172.17.0.2" >> hosts;
+#    * Dockerfile : ssh-copy-id -i ~/.ssh/id_rsa.pub peer@172.17.0.2 < password;
+
 #remove host ip adresses file
 rm -f ip_adresses
 rm -f hosts
@@ -8,18 +12,20 @@ rm -f hosts
 docker run -itd \
 -v "$(pwd):/home/dev" \
 ansible_host;
-echo "172.17.0.2" >> hosts
+echo "172.17.0.2" >> hosts;
 
 # host 2
 docker run -itd \
 -v "$(pwd):/home/dev" \
 ansible_host;
-echo "172.17.0.3" >> hosts
+echo "172.17.0.3" >> hosts;
+
+sleep 2
 
 # main machine
 docker run -it \
+--hostname launcher \
 -v "$(pwd):/home/dev" \
-ansible \
-bash;
+ansible;
 
-./stop_virtual_network.sh
+./stop_virtual_network.sh;
