4100351de924c925da223227737960900f90cfab
Kamlinne-Bebora Ly <kbly@enseirb-matmeca.fr>
2022-03-19 15:57:50 +0100

    Début du Homepage avec pug + Taille des éléments variant en fonction de la taille de la fenêtre

--- serveur/src/server.ts
+++ serveur/src/server.ts
@@ -4,9 +4,10 @@ const app = express();
 const port = 3000;
 
 app.set('views','./views');
+app.use(express.static(__dirname + '/../public'));
 app.set('view engine','pug');
 
-const db = new DatabaseAccess("user", "password");
+const db = new DatabaseAccess("Beborah", "rjth352HFD64t");
 db.addInfo(["P8","P7","P5"]);
 
 app.get('/acceuil',(req, res) => {
