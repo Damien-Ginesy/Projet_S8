a950c1ecc3d32257e15392a716424193e4fbccca
ImadABID <imad.abied@gmail.com>
2022-04-11 03:23:13 +0200

    commenting not yet working code

--- bootstrap_server/bootstrap.c
+++ bootstrap_server/bootstrap.c
@@ -2,12 +2,71 @@
  * This file is part of Basalt Project.
  ******************************************************************************/
 
-#include <stdint.h>
 #include <stdio.h>
-
-typedef struct 
-{
-    /* data */
-}Node_info;
+#include <stdlib.h>
+#include <string.h>
 
 
+#include "bootstrap.h"
+
+// ----- parse_simu_params
+
+// int compute_attacks_nbr(int argc, int **argv){
+//     int comp = 0;
+//     for(int arg_i = 1; arg_i < argc; arg_i++){
+//         if(strcmp(argv[arg_i], "eclipse") == 0 || strcmp(argv[arg_i], "inst") == 0){
+//             comp++;
+//         }
+//     }
+//     return comp;
+// }
+
+// void parse_simu_params(int argc, int **argv){
+
+//     attacks_nbr = compute_attacks_nbr(argc, argv);
+    
+//     attacks_info = malloc(attacks_nbr*sizeof(struct attack_info));
+//     for(int i = 0; i < attacks_nbr; i++){
+//         attacks_info[i].mask = 0;
+//         attacks_info[i].count_member = 0;
+//         attacks_info[i].members = NULL;
+//     }
+
+//     int arg_i = 0;
+//     int attack_i = 0;
+//     while(1){
+//         if(strcmp(argv[arg_i], "eclipse") == 0){
+//             arg_i++;
+
+//             attacks_info[attack_i].id = atoi(argv[arg_i++]);
+
+//             attack_i++;
+
+//         }else if(strcmp(argv[arg_i], "inst") == 0){
+//             arg_i++;
+
+            
+
+//             attack_i++;
+
+//         }else{ // nbr nodes
+
+//             node_nbr = atoi(argv[arg_i]);
+
+//             if(node_nbr == 0){
+//                 fprintf(stderr, "Non valid params\n");
+//                 exit(EXIT_FAILURE);
+//             }
+
+//             if(arg_i+1 >= argc){
+//                 fprintf(stderr, "Non valid params\n");
+//                 exit(EXIT_FAILURE);
+//             }
+
+//             break;
+
+//         }
+//     }
+// }
+
+// -----
--- bootstrap_server/bootstrap.h
+++ bootstrap_server/bootstrap.h
@@ -1,12 +1,27 @@
 #ifndef BOOTSTRAP_H_
 #define BOOTSTRAP_H_
 
-struct Node_info;
-struct Malware_Node;
+#include "exchange_protocol.h"
 
-int NODE_NUM; /* nombre de processus dsm */
-int NODE_ID; /* rang (= numero) du processus */
+struct node_info{
+    struct node_network_info network;
+    int view_size;
+    int attaque_id;
+};
 
+struct attack_info{
+    int id;
+    char mask;
+    unsigned char network_ip[4];
+    int count_member;
+    struct node_network_info *members;
+};
 
+// simu params
+int node_nbr;
+int attacks_nbr;
+struct attack_info *attacks_info;
+
+void parse_simu_params(int argc, int argv);
 
 #endif
