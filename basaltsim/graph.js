68f48e362fc3e2e07552c51d32020a87712ff5c5
Victor FALAISE <fullfox>
2022-04-15 13:17:43 +0200

    update

--- basaltsim/graph.js
+++ basaltsim/graph.js
@@ -136,6 +136,6 @@ class Graph {
   }
 
   static drawInfectionRate(){
-    document.getElementById("arrow").style.transform = "rotate(" + targetViewInfection*360 + "deg)";
+    document.getElementById("arrow").style.transform = "rotate(" + (-targetViewInfection*270 + 180) + "deg)";
   }
 }
