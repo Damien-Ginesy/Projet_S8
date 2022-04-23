5767fb1e7806fe5ddc8dc3120fe1c73450d3ec9f
ImadAbid <imad.abied@gmail.com>
2022-04-23 15:44:54 +0200

    adding structures, init & free

---
+++ bootstrap_server/attack.h
@@ -0,0 +1,40 @@
+#ifndef _ATTACK_H_
+#define _ATTACK_H_
+
+
+struct attack_member{
+    struct node_network_info * net_info;
+    struct attackers_list *next;
+};
+
+struct attack_members_list{
+    int size;
+    struct attack_member *first_member;
+};
+
+
+struct attack_info{
+    int id;
+    char mask;
+    unsigned char network_ip[4];
+    struct attack_members_list *members;
+};
+
+extern int attacks_nbr;
+extern struct attack_info *attacks_info;
+
+
+void attacks_store_by_mask(
+    struct attack_info *attacks_tab,
+    int attacks_tab_size
+);
+
+// attackers list
+
+struct attack_members_list * attack_members_list_new();
+void attack_members_list_free(struct attack_members_list *aml);
+
+
+
+
+#endif
