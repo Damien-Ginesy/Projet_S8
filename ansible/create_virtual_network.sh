8340f735321682d7bfe7e0e8a5b4e1405932c6a4
FELLAH Hicham <hfellah@enseirb-matmeca.fr>
2022-04-25 05:55:55 +0200

    pouvoir du lancement de boostrap dans les hosts et tester sa fonctionnalité, prendre en compte le nbre des machines dynamiques au lancement du docker, reste à utiliser les paramètres dans les commandes docker

--- ansible/create_virtual_network.sh
+++ ansible/create_virtual_network.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+# les arguments du script : 
+# Ansible_command --port 3000 --machine 192.0.0.1 45  --machine 192.0.0.12 35  --machine 192.0.0.111 77 --machine_malicious 192.0.0.0 33 Choice 1 --info 11 12 23 34
 # En cas de modification de nbr de hosts
 #    * create_virtual_network.sh : echo "172.17.0.2" >> hosts;
 #    * Dockerfile : ssh-copy-id -i ~/.ssh/id_rsa.pub peer@172.17.0.2 < password;
@@ -8,17 +10,62 @@
 rm -f ip_adresses
 rm -f hosts
 
-# host 1
-docker run -itd \
--v "$(pwd):/home/dev" \
-ansible_host;
-echo "172.17.0.2" >> hosts;
 
-# host 2
-docker run -itd \
--v "$(pwd):/home/dev" \
-ansible_host;
-echo "172.17.0.3" >> hosts;
+#hosts 
+nbr=$#;
+nbrup=$(($nbr+7));
+# le 7 pour les shifts
+for (( i=0; i<$nbrup; i++ ))
+    do
+        case $1 in
+
+        --port)
+            shift
+            PORT=$1;
+            shift
+            ;;
+
+        --machine)
+            # host 
+            shift
+            echo $1 >> hosts;
+            shift
+            NODE_NBR=$1 
+            docker run -itd  \
+            --hostname hosts \
+            -v "$(pwd):/home/dev" \
+            -e NODE_NBR \
+            ansible_host;
+            shift
+            ;;
+
+        --machine_malicious)
+            # malicious host 
+            shift
+            echo $1 >> hosts;
+            shift
+            j1=$1;
+            shift
+            j2=$1;
+            shift
+            j3=$1;
+            echo "$PORT";
+            docker run -itd \
+            --hostname launcher \
+            -e NODE_NBR=$j1 \
+            -e ATTACK=$j2 \
+            -e ATTACK_ID=$j3 \
+            -e PORT=$PORT \
+            -v "$(pwd):/home/dev" \
+            ansible_host;
+            shift
+            ;;
+        --info)
+            
+            ;;
+        esac
+        
+    done
 
 sleep 2
 
