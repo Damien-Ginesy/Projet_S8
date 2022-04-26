97bc024127eae3ce38420a5221421ea78040a9b4
Damien Ginesy <dginesy@enseirb-matmeca.fr>
2022-04-27 00:56:38 +0200

    fin de l'API noeud serveur

--- serveur/src/db.ts
+++ serveur/src/db.ts
@@ -1,57 +1,57 @@
-import {Schema, model, Model} from "mongoose";
-import mongoose from "mongoose";
+// @ts-ignore
+import mongoose, {model, Model, Schema} from "mongoose";
 import {InfoNoeud} from "./Interface/InfoNoeud";
 
- export class DatabaseAccess {
+export class DatabaseAccess {
 
-    private noeudSchema:Schema = new Schema(
-    {
-        nodeID:{
-            port:{type:Number, required:true},
-            adresseReelle:{type:String, required:true},
-            adresseVirtuelle:{type:Number, required:true},
-        },
-        vue:[{
-            nodeID:{
-                port:{type:String, required:true},
-                adresseReelle:{type:String, required:true},
-                adresseVirtuelle:{type:String, required:true},
+    private noeudSchema: Schema = new Schema(
+        {
+            nodeID: {
+                port: {type: Number, required: true},
+                adresseReelle: {type: String, required: true},
+                adresseVirtuelle: {type: Number, required: true},
             },
-            seed:{type:Number, required: true},
-            hitCount:{type:Number, required:true},
-        }],
-        age:{type:Number, required:false},
-        malicieux:{type:Number, required:false},
-    },
+            vue: [{
+                nodeID: {
+                    port: {type: String, required: true},
+                    adresseReelle: {type: String, required: true},
+                    adresseVirtuelle: {type: String, required: true},
+                },
+                seed: {type: Number, required: true},
+                hitCount: {type: Number, required: true},
+            }],
+            age: {type: Number, required: false},
+            malicieux: {type: Number, required: true},
+        },
     );
 
-    private noeudModel:Model<InfoNoeud> = model<InfoNoeud>('Info_Noeuds', this.noeudSchema);
+    private noeudModel: Model<InfoNoeud> = model<InfoNoeud>('Info_Noeuds', this.noeudSchema);
 
-    constructor(user:string, password:string) {
+    constructor(user: string, password: string) {
         const urlmongo = `mongodb+srv://${user}:${password}@test.bnuu4.mongodb.net/RéseauxData?retryWrites=true&w=majority`;
         /*const urlmongo = 'mongodb+srv://thomas:9jqtaEAfBloSTSam@cluster0.tr1ew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';*/
         this.openDb(urlmongo);
     }
 
-    addInfo(infoNoeud:InfoNoeud){
-        const noeud:InfoNoeud = new this.noeudModel({
-            nodeID:infoNoeud.nodeID,
-            vue:infoNoeud.vue,
+    addInfo(infoNoeud: any) {
+        const noeud: InfoNoeud = new this.noeudModel({
+            nodeID: infoNoeud.nodeID,
+            vue: infoNoeud.vue,
             malicieux: infoNoeud.malicieux,
         });
-        noeud.save();
+        noeud.save().then(() => console.log("Ajout réussi\n"));
     }
 
-    recupNoeud(infoNoeud:InfoNoeud):Promise<any>{
-        return this.noeudModel.findOne({noeudID:infoNoeud.nodeID}).exec();
+    recupNoeud(infoNoeud: any): Promise<any> {
+        return this.noeudModel.findOne({nodeID: infoNoeud.nodeID}).exec();
     }
 
-    async updateNoeud(noeud: InfoNoeud) {
-        const noeudEnregistrer:InfoNoeud = await this.recupNoeud(noeud);
+    async updateNoeud(noeud: any) {
+        const noeudEnregistrer: InfoNoeud = await this.recupNoeud(noeud);
         noeudEnregistrer.vue = noeud.vue;
         noeudEnregistrer.age = noeud.age;
         noeudEnregistrer.malicieux = noeud.malicieux;
-        noeudEnregistrer.save();
+        noeudEnregistrer.save().then(() => console.log("Modification réussi\n"));
     }
 
     private openDb(url: string) {
@@ -62,4 +62,4 @@ import {InfoNoeud} from "./Interface/InfoNoeud";
             console.log("Connexion à la base de donnée réussi");
         });
     }
- }
+}
--- serveur/src/server.ts
+++ serveur/src/server.ts
@@ -1,17 +1,18 @@
+// @ts-ignore
 import express from 'express';
 import {DatabaseAccess} from './db'
 import {InfoNoeud} from "./Interface/InfoNoeud";
 
 const app = express();
-const port:number = 3000;
+const port: number = 3000;
 
-app.set('views','./views');
+app.set('views', './views');
 app.use(express.static(__dirname + '/../public'));
 app.use(express.json());
-app.set('view engine','pug');
+app.set('view engine', 'pug');
 
 
-if(process.argv.length !== 4){
+if (process.argv.length !== 4) {
     console.error("Le programme attend en argument un User et un password pour la base de donnée\n");
     process.exit();
 }
@@ -19,40 +20,42 @@ if(process.argv.length !== 4){
 
 const db = new DatabaseAccess(process.argv[2], process.argv[3]);
 
-app.post('/infoNoeud',async (req, res) => {
-    console.log(req.body);
-    const existe: InfoNoeud | undefined = await db.recupNoeud(req.body[0]);
-    if(existe === undefined){
-        db.addInfo(req.body[0]);
-    }
-    else{
-        await db.updateNoeud(req.body);
+app.post('/infoNoeud', async (req, res) => {
+
+    let noeud: any;
+    for (noeud of req.body) {
+        console.log(noeud);
+        const existe: InfoNoeud | null = await db.recupNoeud(noeud);
+        if (existe === null) {
+            db.addInfo(noeud);
+        } else {
+            await db.updateNoeud(noeud);
+        }
     }
     res.sendStatus(200);
 })
 
-app.get('/network', (req,res)=>{
+app.get('/network', (req, res) => {
     res.render('network');
 })
 
-app.get('/stats', (req,res)=>{
+app.get('/stats', (req, res) => {
     res.render('stats');
 })
 
-app.get('/accueil',(req, res) => {
+app.get('/accueil', (req, res) => {
     res.render('homepage');
 })
 
-app.get('/stats',(req, res) => {
+app.get('/stats', (req, res) => {
     res.render('stats');
 })
 
-app.get('/',(req, res) =>{
+app.get('/', (req, res) => {
     res.redirect('/accueil');
 });
 
 
-
-app.listen(port ,() => {
+app.listen(port, () => {
     console.log(`Server is listening on ${port}`);
 });
