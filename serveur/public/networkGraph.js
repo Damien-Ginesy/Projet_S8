4c54dd2092068e6d76d5ebf76ca8dbc9ab341e42
Kamlinne-Bebora Ly <kbly@enseirb-matmeca.fr>
2022-04-30 17:46:57 +0200

    Revenir en arrière et reliage des noeuds

--- serveur/public/networkGraph.js
+++ serveur/public/networkGraph.js
@@ -2,7 +2,7 @@ const pcd = document.getElementById('topNavLeft');
 const popupCross = document.getElementById('popupQuit');
 
 class NetworkGraph {
-    constructor(nodeNb,nodeFinalMax,infectedNodeNb,viewSize){
+    constructor(nodeNb,nodeFinalMax,infectedArray,viewArray){
         this.canvas = document.querySelector("#particles-js");
         this.ctx = this.canvas.getContext('2d');
         this.canvas.width = window.innerWidth;
@@ -11,11 +11,10 @@ class NetworkGraph {
         this.nodeNb = nodeNb;
         this.nodeFinalMax = nodeFinalMax;
         this.gpWin = 0;
-        this.infectedNodeNb = infectedNodeNb;
-        this.viewSize = viewSize;
+        this.viewArray = viewArray;
         this.nodesWin = [];
         this.nodes = [];
-        this.infectedList = [];
+        this.infectedArray = infectedArray;
     }
 
     init(){
@@ -36,7 +35,7 @@ class NetworkGraph {
         const nodes = nodesWinInfo.nodeNbInGrp;
         if (nodes>this.nodeFinalMax){
             let subGrp;
-            Math.floor(nodes/4)<100 ? subGrp=2 : subGrp = 4;
+            Math.floor(nodes/4)<225 ? subGrp=2 : subGrp = 4;
             const nodesInGpMean = Math.floor(nodes/subGrp);
             const nodeRemain = nodes%subGrp;
             const nodesNewWinInfo = [];
@@ -78,7 +77,13 @@ class NetworkGraph {
         const firstId = winFinal ? nodesWin[0].firstId : 0;
         for (let i=0;i<end;i++){
             const idx = i+firstId;
-            const node = Node.nodeInit(idx,cvsDim, this.nodes, end, winFinal);
+            const infect = (winFinal && this.infectedArray.includes(idx))?1:0;
+            const view = winFinal?this.viewArray[idx]:this.nodes;
+            const info = {
+                'infect': infect,
+                'view':view,
+            }
+            const node = Node.nodeInit(idx,cvsDim, this.nodes, end, winFinal, info);
             this.nodes.push(node);
         }
     }
@@ -90,9 +95,16 @@ class NetworkGraph {
     }
 
     addWin(nodeWin){
+        if (this.nodesWin[this.gpWin].length===1)
+            return;
         this.deleteNodesArray();
         this.checkSetWindow(nodeWin);
         this.nodesSet(this.nodesWin[this.gpWin]);
+    }
+
+    deleteNodesWinArray(){
+        this.nodesWin.splice(this.gpWin);
+        this.gpWin--;
     }
 
     resizeCanvas(){
@@ -108,14 +120,16 @@ class NetworkGraph {
             // Collision entre noeuds
             for (let j=i+1;j<this.nodes.length;j++){
                 if (Node.checkCollision(this.nodes[i].circle,this.nodes[j].circle)){
-                    Node.processCollision(this.nodes[i],this.nodes[j]);
+                    Node.processCollision(this.nodes[i].circle,this.nodes[j].circle);
                 }
             }
             const cvsDim = {
                 'width': this.canvas.width,
                 'height': this.canvas.height
             }
+
             Node.nodeDraw(this.nodes[i], this.ctx);
+            Node.nodeDrawLine(i,this.nodes, this.ctx, this.nodesWin[this.gpWin]);
             Node.nodeMove(this.nodes[i], cvsDim);
         }
         requestAnimationFrame(this.update.bind(this));
@@ -136,14 +150,14 @@ function mousePosition(event) {
 function getInfectedList(nbInfectedNode, nbNodeTot){
     const infectList = [];
     while(infectList.length!==nbInfectedNode){
-      const infectedNodeId = Math.floor(Math.random() * nbNodeTot)+1;
+      const infectedNodeId = Math.floor(Math.random() * nbNodeTot);
       if (!infectList.includes(infectedNodeId))
         infectList.push(infectedNodeId);
     }
     return infectList;
   }
   
-  function getViewList(viewSize, nbNodeTot){
+function getViewList(viewSize, nbNodeTot){
     const viewList = [];
     for (let i=0;i<nbNodeTot;i++){
       const viewNodeList = [];
@@ -157,12 +171,15 @@ function getInfectedList(nbInfectedNode, nbNodeTot){
     return viewList;
 }
 
-const nbNodeTot = 1000;
+const nbNodeTot = 100;
 const nodeFinalMax = 200;
 const nbInfectedNode = 4;
 const viewSize = 5;
 
-c = new NetworkGraph(nbNodeTot,nodeFinalMax,nbInfectedNode,viewSize);
+const viewArray = getViewList(viewSize, nbNodeTot);
+const infectArray = getInfectedList(nbInfectedNode ,nbNodeTot);
+
+c = new NetworkGraph(nbNodeTot,nodeFinalMax,infectArray,viewArray);
 c = c.init();
 c.update();
 
@@ -175,7 +192,9 @@ const breakException = {};
 pcd.addEventListener('click', function(event) {
     if(c.gpWin<2)
         return;
-    console.log('more')
+    c.deleteNodesArray();
+    c.deleteNodesWinArray();
+    c.nodesSet(c.nodesWin[c.gpWin]);
     }, false
 )
 
@@ -186,11 +205,16 @@ popupCross.addEventListener('click', function(event) {
 c.canvas.addEventListener("mousemove", function(event){
     const mouse = mousePosition(event);
     const nodeWin = c.nodesWin[c.gpWin];
+    const final = nodeWin.length===1?1:0;
     try{
-        c.nodes.forEach(el=>{
-            if(Node.mouseInNode(el.circle, mouse)){
-                const final = nodeWin.length===1?1:0;
+        c.nodes.forEach((el, idx)=>{
+            if(Node.mouseInNode(el.circle, mouse) && !final){
                 nodeHover(nodeWin[el.id],final);
+                throw breakException;
+            }
+            else if (Node.mouseInNode(el.circle, mouse) && final){
+                //Node.nodeHighlight(idx,c.nodes, c.ctx);
+                nodeHover(el,final);
                 throw breakException;
             }
         })
@@ -209,10 +233,12 @@ c.canvas.addEventListener('click', function(event){
         c.nodes.forEach(el=>{
             if(Node.mouseInNode(el.circle, mouse)){
                 c.addWin(nodeWin[el.id]);
+                throw breakException;
             }
         })
    }catch(err){
-       console.log(err);
+        if(err!== breakException)
+            throw err   
    }}, false
 )
 
--- serveur/public/networkNode.js
+++ serveur/public/networkNode.js
@@ -9,30 +9,32 @@ class Node {
         this.view = view;
     } */
 
-    constructor(id,circle, sx, sy) {
+    constructor(id,circle,info) {
         this.id = id;
         this.circle = circle;
-        this.sx = sx;
-        this.sy = sy;
+        this.info = info
     }
 
-    static nodeInit(id, cvsDim,nodes, n, winFinal){
+    static nodeInit(id, cvsDim,nodes, n, winFinal, info){
         let circle;
-        let xspd,yspd;
         if (winFinal){
+
+            /* Si circulaire */ /* modifier la répartition */
             circle = Node.setPosition(id, cvsDim,'circular', n);
-            xspd = 0,
-            yspd = 0;
+            /* si aléatoire */ /* enlever n + winFinal et le remplacer par r */
+            /* circle = Node.setPosition(id, cvsDim,'default',20);
+            while(this.checkBorder(circle, cvsDim) || this.checkOverlap(circle,nodes)){
+                // changer l'argument
+                circle = Node.setPosition(id, cvsDim,'default',20);
+            } */
         }
         else{
             circle = Node.setPosition(id, cvsDim,'default',n);
             while(this.checkBorder(circle, cvsDim) || this.checkOverlap(circle,nodes)){
                 circle = Node.setPosition(id, cvsDim,'default',n);
             }
-            xspd = Math.floor((Math.random()*0.2))+0.5;
-            yspd = Math.floor((Math.random()*0.2))+0.5;
         }
-        const c=new Node(id,circle,xspd,yspd);
+        const c=new Node(id,circle, info);
         return c;
     }
 
@@ -48,13 +50,16 @@ class Node {
                 const phi = angle * i * 2;
                 const cx = cvsDim.width/2+(baseRadius + r) * Math.cos(phi);
                 const cy = cvsDim.height/2+(baseRadius + r) * Math.sin(phi);
-                circle = {'x' : cx, 'y' : cy, 'r': r};
+                circle = {'x' : cx, 'y' : cy, 'r': r, 'sx': 0, 'sy': 0};
                 return circle;
             default:
-                const rad = Math.min(cvsDim.height,cvsDim.width)/10,
+                /* A MODIFIER */
+                const rad = n===20 ? n : Math.min(cvsDim.height,cvsDim.width)/10,
+                    xspd = n===20 ? 0 : Math.floor((Math.random()*0.2))+0.5,
+                    yspd = n===20 ? 0 : Math.floor((Math.random()*0.2))+0.5,
                     x = Math.floor((Math.random()*cvsDim.width)),
                     y = Math.floor((Math.random()*cvsDim.height));
-                circle = {'x' : x, 'y' :y, 'r': rad};
+                circle = {'x' : x, 'y' :y, 'r': rad, 'sx': xspd, 'sy': yspd};
                 return circle;
         }
     }
@@ -102,24 +107,24 @@ class Node {
       }
 
     static nodeMove(node, cvsDim){
-        node.circle.x += node.sx;
-        node.circle.y += node.sy;
+        node.circle.x += node.circle.sx;
+        node.circle.y += node.circle.sy;
 
         if (node.circle.x + node.circle.r > cvsDim.width) {
             node.circle.x = cvsDim.width - node.circle.r;
-            node.sx *= -1;
+            node.circle.sx *= -1;
         }
         else if (node.circle.x - node.circle.r < 0) {
             node.circle.x = node.circle.r;
-            node.sx *= -1;
+            node.circle.sx *= -1;
         }
         if (node.circle.y + node.circle.r > cvsDim.height) {
             node.circle.y = cvsDim.height - node.circle.r;
-            node.sy *= -1;
+            node.circle.sy *= -1;
         }
         else if (node.circle.y - node.circle.r < 0) {
             node.circle.y = node.circle.r;
-            node.sy *= -1;
+            node.circle.sy *= -1;
         }
 
     }
@@ -128,10 +133,60 @@ class Node {
         ctx.beginPath();        
         ctx.globalAlpha = 0.5;
         ctx.arc(node.circle.x, node.circle.y,node.circle.r, 0, 2 * Math.PI);
-        ctx.fillStyle = "#000000";
+        ctx.fillStyle = node.info.infect? '#C70039' : "#000000";
         ctx.fill();
         ctx.closePath(); 
     }
+
+    static nodeDrawLine(idx,nodes, ctx,win){
+        const winFinal = win.length === 1;
+        if (!winFinal){
+            nodes[idx].info.view.forEach(element => {
+                if (element.id === nodes[idx].id)
+                    return;
+                
+                const mx = nodes[idx].circle.x;
+                const my= nodes[idx].circle.y;
+                ctx.globalAlpha = 0.1;
+                ctx.beginPath();
+                ctx.moveTo(element.circle.x,element.circle.y);
+                ctx.lineTo(mx,my);
+                ctx.stroke();
+                ctx.closePath();   
+            });
+        }
+        else{
+            nodes[idx].info.view.forEach(element => {
+                if (element < win[0].firstId || element >= win[0].firstId+win[0].nodeNbInGrp)
+                    return;
+                const mx = nodes[idx].circle.x;
+                const my= nodes[idx].circle.y;
+                const nodeIdx = element - win[0].firstId;
+                ctx.globalAlpha = 0.1;
+                ctx.beginPath();
+                ctx.moveTo(nodes[nodeIdx].circle.x,nodes[nodeIdx].circle.y);
+                ctx.lineTo(mx,my);
+                ctx.stroke();
+                ctx.closePath();   
+            });
+        }
+    }
+
+    /* static nodeHightlight(idx,nodes, ctx){
+        nodes[idx].info.view.forEach(element => {
+            if (element < win[0].firstId || element >= win[0].firstId+win[0].nodeNbInGrp)
+                return;
+            const mx = nodes[idx].circle.x;
+            const my= nodes[idx].circle.y;
+            const nodeIdx = element - win[0].firstId;
+            ctx.globalAlpha = 0.1;
+            ctx.beginPath();
+            ctx.moveTo(nodes[nodeIdx].circle.x,nodes[nodeIdx].circle.y);
+            ctx.lineTo(mx,my);
+            ctx.stroke();
+            ctx.closePath();   
+        });
+    } */
 
     static mouseInNode(node, mouse){
         const dx = node.x - mouse.x;
