e36d60c62f1b369f2b515be1c04dda8deb2d2be5
Maël VIRIOT <mviriotlecon@enseirb-matmeca.fr>
2022-05-02 09:07:30 +0200

    HTTPClient test

--- core/test2.cpp
+++ core/test2.cpp
@@ -1,34 +1,23 @@
-#include <NodeId.hpp>
-#include <net/Message.hpp>
-#include <array>
-#include <iostream>
-#include <Array.hpp>
+#include <basalt.hpp>
+#include <semaphore>
+
+std::binary_semaphore sem(0);
+
+void on_logger_resp(const llhttp_t& parser, Basalt::net::HTTPClient::BufferView body){
+    std::cout << "Code: " << parser.status_code << '\n';
+    for(char c: body)
+        std::cout << c;
+    sem.release();
+}
 
 int main(int argc, char const *argv[])
 {
     using namespace Basalt;
-    NodeId id = {asio::ip::address_v4(0x7f000001), 2048, 0x00010002};
-    NodeId::bytes_t d1 = {};
-    id.to_bytes(d1);
-    for(unsigned char b: d1)
-        printf("%02x ", b);
-    printf("\n");
-    NodeId::bytes_t d2 = {
-        127, 0, 0, 1,
-        0, 8,
-        2, 0, 1, 0
-    };
-    NodeId id2 = NodeId::from_bytes(d2);
-    std::cout << id2.to_string() << '\n';
+    using namespace std::chrono_literals;
 
-    net::Message msg;
-    msg << id._addr.to_bytes() << id._port << id.id;
+    net::HTTPClient cli("example.org");
 
-    std::array<byte, 10> out;
-    msg >> out;
-    for(byte b: out)
-        std::printf("%02x ", b);
-    printf("\n%u\n", msg.payloadSize());
-
+    cli.GET("/", on_logger_resp);
+    sem.acquire();
     return 0;
 }
