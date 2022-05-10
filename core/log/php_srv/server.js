78e8a210f3b8c1cac3a7ea447cd7d4266698bee0
fullfox <fullfox@medusa.fr>
2022-05-10 16:07:59 +0200

    update p2p_test + basalt log delayed uniform

---
+++ core/log/php_srv/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 8081;
+
+var database = {};
+var s = false;
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  if(!s){s = true; console.log("Started!")}
+
+  let data = req.body;
+  let id = data[0]["nodeID"]["adresseVirtuelle"];
+  database[id] = data[0];
+
+  res.send();
+})
+
+app.get('/log.log', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.send(JSON.stringify(database));
+})
+
+
+app.listen(port, () => {
+  console.log(`Logging server running on port ${port}`)
+})
