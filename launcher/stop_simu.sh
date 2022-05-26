28902cbc76c132dfffa143b755a187cd1fa04a53
ImadAbid <imad.abied@gmail.com>
2022-05-26 13:56:05 +0200

    killing basalt nodes correctly

--- launcher/stop_simu.sh
+++ launcher/stop_simu.sh
@@ -2,7 +2,7 @@
 
 pkill bootstrap_serve;
 
-# ansible all -a "pkill honest_node; pkill malicious_node;"
+su peer -c 'ansible all -a "pkill honest_node; pkill malicious_node;"';
 
 # kill metric server
 kill $(lsof -t -i :3000);
