b0605644f77a924783bce431297b69c077ec34ed
ImadAbid <imad.abied@gmail.com>
2022-05-29 04:49:12 +0200

    fixing parallel execution of in_host_launcher

--- ansible/in_host_launcher.sh
+++ ansible/in_host_launcher.sh
@@ -15,7 +15,8 @@ while [ "$i" -lt $2 ];
 do
     i=$(expr $i + 1);
     
-    port="$(echo "basalt" | su root -c "/home/peer/find_free_port.sh > port" > /dev/null 2> /dev/null; cat port)";
+    port_store_filename=$((1 + $RANDOM))
+    port="$(echo "basalt" | su root -c "/home/peer/find_free_port.sh > $port_store_filename" > /dev/null 2> /dev/null; cat $port_store_filename; rm $port_store_filename;)";
 
     if [ $1 -eq "0" ]; then
         /home/peer/bin/honest_node $3 $port $4 $5 $6 $7 $8 $7 3000 > /dev/null &
