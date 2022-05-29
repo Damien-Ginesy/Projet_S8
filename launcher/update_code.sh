3ab99fa208aaee624f7bf7765c17476419582e07
ImadAbid <imad.abied@gmail.com>
2022-05-29 03:14:08 +0200

    putting bins in the right path directly after compilation

--- launcher/update_code.sh
+++ launcher/update_code.sh
@@ -7,6 +7,9 @@ cd /home/Projet_S8/bootstrap_server/; make > /dev/null;
 
 cd /home/Projet_S8/; cmake -B build/ > /dev/null; cmake --build build/ > /dev/null;
 
+cp /home/Projet_S8/bootstrap_server/bin/bootstrap_server /home/Projet_S8/bin/bootstrap_server;
+cp -r /home/Projet_S8/bin/ /home/peer/bin/;
+
 cd /home/Projet_S8/launcher/; npm install > /dev/null;
 cd /home/Projet_S8/serveur/; npm install > /dev/null;
 
