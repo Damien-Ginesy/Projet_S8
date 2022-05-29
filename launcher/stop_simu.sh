5963d66865f456032970bb9adf6b0c45a9173598
ImadAbid <imad.abied@gmail.com>
2022-05-29 03:49:43 +0200

    launcher.js

--- launcher/stop_simu.sh
+++ launcher/stop_simu.sh
@@ -2,9 +2,8 @@
 
 pkill bootstrap_serve;
 
-su peer -c 'ansible all -a "pkill honest_node"';
-
-su peer -c 'ansible all -a "pkill malicious_node"';
+pkill honest_node;
+pkill malicious_node;
 
 # kill metric server
 kill $(lsof -t -i :3000);
